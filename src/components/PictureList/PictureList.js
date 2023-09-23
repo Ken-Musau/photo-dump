32ca417ca6fedc28957603cbe5e0ea149a74089a
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-24 02:44:40 +0300

    style search

--- src/components/PictureList/PictureList.js
+++ src/components/PictureList/PictureList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./pictureList.css";
 import { PictureData } from "../../hooks/usePictureData";
+import SearchBox from "../SearchBox/SearchBox";
 
 function pictureList() {
   const { data, isLoading, isError, error } = PictureData();
@@ -10,15 +11,18 @@ function pictureList() {
   if (isLoading) return <h1>Loading ....</h1>;
   if (isError) return <h1>{error.message}</h1>;
   return (
-    <div className="picture-list">
-      <ul className="picture-grid-list">
-        {data?.data.map((picture, index) => (
-          <li key={index}>
-            <img src={picture.urls.small} alt="office" className="pic-self" />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SearchBox />
+      <div className="picture-list">
+        <ul className="picture-grid-list">
+          {data?.data.map((picture, index) => (
+            <li key={index}>
+              <img src={picture.urls.small} alt="office" className="pic-self" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
